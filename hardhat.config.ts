f220f3d2428f62c31b3a472a97efe31b152408f6
Daniel Sintimbrean <ddsintim@inf.upv.es>
2023-01-07 21:42:57 +0100

    add prettier with solidity plugin and fix ts-config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,8 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-};
+  solidity: '0.8.17',
+}
 
-export default config;
+export default config
