c0830524971fd16c2823de19e7f547e3c8b7a4b4
Daniel Sintimbrean <ddsintim@inf.upv.es>
2023-01-09 16:41:07 +0100

    Install zod and create a better treatment of  env variables

---
+++ env/index.ts
@@ -0,0 +1,31 @@
+import { z, ZodFormattedError } from "zod";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const envSchema = z.object({
+  MRC_BLOCKNUMBER: z.coerce.number().default(35079287),
+  ALCHEMY_POLYGON: z.string().url().default("https://polygon-rpc.com/"),
+});
+
+export const formatErrors = (
+  errors: ZodFormattedError<Map<string, string>, string>,
+) =>
+  Object.entries(errors)
+    .map(([name, value]) => {
+      if (value && "_errors" in value)
+        return `${name}: ${value._errors.join(", ")}\n`;
+    })
+    .filter(Boolean);
+
+const _env = envSchema.safeParse(process.env);
+
+if (!_env.success) {
+  console.error(
+    "❌ Invalid environment variables:\n",
+    ...formatErrors(_env.error.format()),
+  );
+  throw new Error("Invalid environment variables");
+}
+
+export const env = _env.data;
